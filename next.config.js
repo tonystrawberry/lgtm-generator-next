f550cb227c7238149642a63f28f070ef58f37067
Tony Duong <tony.duong.102@gmail.com>
2023-11-05 21:28:31 +0900

    :sparkles: add domains in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["d196qgnoqlk55t.cloudfront.net"],
     remotePatterns: [
       {
         protocol: 'https',
