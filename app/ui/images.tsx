8dc565e5bea62d1ba8fc20de0e8190ea18c7e466
Tony Duong <tony.duong.102@gmail.com>
2023-11-13 21:19:14 +0900

    :sparkles: add search bar

--- app/ui/images.tsx
+++ app/ui/images.tsx
@@ -8,6 +8,8 @@ import { LgtmImage } from "@/lib/definitions";
 import clsx from "clsx";
 import CustomImage from "@/components/custom-image";
 import { Icons } from "@/components/icons";
+import { Input } from "@/components/ui/input";
+import { useDebounce, useDebouncedCallback } from "use-debounce";
 
 /**
  * Images is the component that renders the gallery of images.
@@ -20,6 +22,7 @@ export default function Images() {
   const [hasMore, setHasMore] = useState<boolean>(false); // Whether there are more images to load.
   const [loading, setLoading] = useState<boolean>(false); // Whether the images are currently loading.
   const [favorites, setFavorites] = useState<string[]>([]); // The list of favorite images.
+  const [search, setSearch] = useState<string | undefined>(undefined); // The search query to use for filtering images.
 
   // Fetch images on first render and initialize favorites from local storage.
   useEffect(() => {
@@ -41,13 +44,19 @@ export default function Images() {
    *
    * @param cursor The cursor to use for the next page of images.
    */
-  const getImages = async (cursor: string | null = null) => {
+  const getImages = async (cursor: string | null = null, keywords: string | undefined = undefined) => {
     if (loading) {
       return;
     }
     setLoading(true);
 
     let url = process.env.NEXT_PUBLIC_SERVER_API_URL as string; // The API endpoint to fetch images from.
+
+    // If there are keywords, add them to the URL.
+    if (keywords) {
+      setImages([]);
+      url = `${url}?keywords=${keywords}`;
+    }
 
     // If there is a cursor, add it to the URL.
     if (cursor) {
@@ -62,7 +71,13 @@ export default function Images() {
       has_more: boolean;
     };
 
-    setImages([...images, ...data.items]);
+    // If there are keywords, replace the images with the new images.
+    // If there are no keywords, append the new images to the existing images.
+    if (keywords) {
+      setImages(data.items);
+    } else {
+      setImages([...images, ...data.items]);
+    }
     setNextCursor(data.next_cursor_created_at);
     setHasMore(data.has_more);
     setLoading(false);
@@ -128,46 +143,67 @@ export default function Images() {
     }
   };
 
+  /**
+   * Handles the search input change event.
+   *
+   * @param event The change event.
+   */
+  const onSearchChange = useDebouncedCallback((search: string) => {
+    if (search === "") {
+      setImages([]);
+    }
+
+    setNextCursor(null);
+    getImages(null, search);
+    initializeFavorites();
+
+    setSearch(search);
+  }, 300);
+
   return (
-    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {images &&
-        images.map((image: any, index: any) => (
-          <div
-            key={index}
-            className="rounded overflow-hidden relative cursor-pointer max-h-[315px]"
-            onClick={() => copyToClipboard(image.image_url)}
-          >
-            <div className="absolute inset-0 bg-white opacity-0 hover:opacity-50 transition-opacity duration-300 z-10">
-              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <Copy size={64} color="black" />
-              </div>
-            </div>
-
+    <div className="p-8">
+      {/* Only show search bar if WITH_SEARCH is set to true */}
+      { process.env.NEXT_PUBLIC_WITH_SEARCH === "true" && <Input className="mb-8" placeholder="Baseball.." onChange={(e) => onSearchChange(e.target.value)}/> }
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {images &&
+          images.map((image: any, index: any) => (
             <div
-              className={clsx(
-                "absolute rounded-full p-2 opacity-80 m-4 hover:opacity-100 z-20",
-                {
-                  "bg-red-600": favorites.includes(image.id),
-                  "bg-white": !favorites.includes(image.id),
-                }
-              )}
-              onClick={(e) => toggleFavorite(e, image.id)}
+              key={index}
+              className="rounded overflow-hidden relative cursor-pointer max-h-[315px]"
+              onClick={() => copyToClipboard(image.image_url)}
             >
-              <Heart
-                size={20}
-                color={favorites.includes(image.id) ? "white" : "red"}
-              />
+              <div className="absolute inset-0 bg-white opacity-0 hover:opacity-50 transition-opacity duration-300 z-10">
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                  <Copy size={64} color="black" />
+                </div>
+              </div>
+
+              <div
+                className={clsx(
+                  "absolute rounded-full p-2 opacity-80 m-4 hover:opacity-100 z-20",
+                  {
+                    "bg-red-600": favorites.includes(image.id),
+                    "bg-white": !favorites.includes(image.id),
+                  }
+                )}
+                onClick={(e) => toggleFavorite(e, image.id)}
+              >
+                <Heart
+                  size={20}
+                  color={favorites.includes(image.id) ? "white" : "red"}
+                />
+              </div>
+
+              <CustomImage src={image.image_url} alt="LGTM" width={500} height={400} className="p-2 z-0" />
             </div>
+          ))}
 
-            <CustomImage src={image.image_url} alt="LGTM" width={500} height={400} className="p-2 z-0" />
+        {loading && (
+          <div className="col-span-3 flex justify-center items-center mt-16">
+            <Icons.spinner className="mr-2 h-16 w-16 animate-spin" />
           </div>
-        ))}
-
-      {loading && (
-        <div className="col-span-3 flex justify-center items-center mt-16">
-          <Icons.spinner className="mr-2 h-16 w-16 animate-spin" />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
